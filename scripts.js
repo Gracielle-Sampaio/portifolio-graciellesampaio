dcc89f01ea320ab73ecbdbe60ee45f3f0154d85b
Gracielle Sampaio <100385174+Gracielle-Sampaio@users.noreply.github.com>
2024-05-18 16:00:56 -0300

    site pc

---
+++ scripts.js
@@ -0,0 +1,59 @@
+const capa1 = document.getElementById('capa1');
+    capa1.addEventListener('click', function() {
+    window.location.href = '/paginas/blasfemia.html';
+    });
+const capa2 = document.getElementById('capa2');
+    capa2.addEventListener('click', function() {
+    window.location.href = '/paginas/projeto2.html';
+    });
+const capa3 = document.getElementById('capa3');
+    capa3.addEventListener('click', function() {
+    window.location.href = '/paginas/ocs.html';
+    });
+
+const blasfemia = document.getElementById('blasfemia');
+    blasfemia.addEventListener('click', function() {
+    window.location.href = '/paginas/blasfemia.html';
+    });
+const lobo = document.getElementById('lobo');
+    lobo.addEventListener('click', function() {
+    window.location.href = '/paginas/lobo.html';
+    });
+const iris = document.getElementById('iris');
+    iris.addEventListener('click', function() {
+    window.location.href = '/paginas/iris.html';
+    });
+const janine = document.getElementById('janine');
+    janine.addEventListener('click', function() {
+    window.location.href = '/paginas/janine.html';
+    });
+const lia = document.getElementById('lia');
+    lia.addEventListener('click', function() {
+    window.location.href = '/paginas/lia.html';
+    });
+const ocs = document.getElementById('ocs');
+    ocs.addEventListener('click', function() {
+    window.location.href = '/paginas/ocs.html';
+    });
+const avulso = document.getElementById('avulso');
+    avulso.addEventListener('click', function() {
+    window.location.href = '/paginas/avulso.html';
+    });
+const pixelart = document.getElementById('pixelart');
+    pixelart.addEventListener('click', function() {
+    window.location.href = '/paginas/pixelart.html';
+    });
+const projeto1 = document.getElementById('projeto1');
+    projeto1.addEventListener('click', function() {
+    window.location.href = '/paginas/projeto1.html';
+    });
+const projeto2 = document.getElementById('projeto2');
+    projeto2.addEventListener('click', function() {
+    window.location.href = '/paginas/projeto2.html';
+    });
+const projeto3 = document.getElementById('projeto3');
+    projeto3.addEventListener('click', function() {
+    window.location.href = '/paginas/projeto3.html';
+    });
+
+
